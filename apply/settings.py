fb4604119f9ff7acd844a727d801eda160bc612a
Dayo Osikoya <alwaysdeone@gmail.com>
2016-03-27 02:08:02 +0100

    django-registration settings and templates.

--- apply/settings.py
+++ apply/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'apply.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+
+# App-specific settings
+ACCOUNT_ACTIVATION_DAYS = 7
