968933dc0c0f5b3e0e5db988b4027c5cba7d306a
Dayo Osikoya <alwaysdeone@gmail.com>
2016-03-29 21:00:40 +0100

    Remove username field from form.

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,5 +3,14 @@ from django import forms
 from registration.forms import RegistrationForm
 
 class ApplyRegistrationForm(RegistrationForm):
+
     first_name = forms.CharField(label='First Name')
     last_name = forms.CharField(label='Last Name')
+
+    def __init__(self, *args, **kwargs):
+        super(ApplyRegistrationForm, self).__init__(*args, **kwargs)
+        self.fields.pop('username')
+
+    def save(self):
+        self.instance.username = self.cleaned_data['email']
+        return super(ApplyRegistrationForm, self).save()
