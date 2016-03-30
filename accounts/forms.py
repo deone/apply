6be86cc35dd2898b8d64c758deda3d2ee769428d
Dayo Osikoya <alwaysdeone@gmail.com>
2016-03-30 21:58:25 +0100

    Form/page design.

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,12 +4,15 @@ from registration.forms import RegistrationForm
 
 class ApplyRegistrationForm(RegistrationForm):
 
-    first_name = forms.CharField(label='First Name')
-    last_name = forms.CharField(label='Last Name')
+    first_name = forms.CharField(label='First Name', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_name = forms.CharField(label='Last Name', widget=forms.TextInput(attrs={'class': 'form-control'}))
 
     def __init__(self, *args, **kwargs):
         super(ApplyRegistrationForm, self).__init__(*args, **kwargs)
         self.fields.pop('username')
+        self.fields['email'].widget = forms.TextInput(attrs={'class': 'form-control'})
+        self.fields['password1'].widget = forms.TextInput(attrs={'class': 'form-control'})
+        self.fields['password2'].widget = forms.TextInput(attrs={'class': 'form-control'})
 
     def save(self, commit=False):
         self.instance.username = self.cleaned_data['email']
