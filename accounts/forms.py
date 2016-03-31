272332a353f7aab6ea8e50cd9ca8bda7fd90ce2c
Dayo Osikoya <alwaysdeone@gmail.com>
2016-03-31 16:40:50 +0100

    - Add translation to LoginForm and ApplyRegistrationForm. - Change title.

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,18 +1,19 @@
 from django import forms
+from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.forms import AuthenticationForm
 
 from registration.forms import RegistrationForm
 
 class LoginForm(AuthenticationForm):
-    username = forms.EmailField(label='Email', max_length=254,
+    username = forms.EmailField(label=_('Email'), max_length=254,
         widget=forms.EmailInput(attrs={'class': 'form-control'}))
-    password = forms.CharField(label='Password',
+    password = forms.CharField(label=_('Password'),
         widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
 class ApplyRegistrationForm(RegistrationForm):
 
-    first_name = forms.CharField(label='First Name', widget=forms.TextInput(attrs={'class': 'form-control'}))
-    last_name = forms.CharField(label='Last Name', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    first_name = forms.CharField(label=_('First Name'), widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_name = forms.CharField(label=_('Last Name'), widget=forms.TextInput(attrs={'class': 'form-control'}))
 
     def __init__(self, *args, **kwargs):
         super(ApplyRegistrationForm, self).__init__(*args, **kwargs)
