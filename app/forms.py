a4ff9d3328a242d0e5ef841827b991537ce331ed
Dayo Osikoya <alwaysdeone@gmail.com>
2016-03-31 23:37:53 +0100

    Complete display of PersonalInformationForm

--- app/forms.py
+++ app/forms.py
@@ -13,3 +13,6 @@ class PersonalInformationForm(forms.ModelForm):
         super(PersonalInformationForm, self).__init__(*args, **kwargs)
         self.fields['date_of_birth'].widget = forms.TextInput(attrs={'class': 'form-control'})
         self.fields['gender'].widget = forms.RadioSelect(choices=PersonalInformation.GENDER_CHOICES)
+        self.fields['applied_before'].widget = forms.RadioSelect(choices=PersonalInformation.BOOL_CHOICES)
+        self.fields['applied_before'].label = 'Have you applied to Ashesi before?'
+        self.fields['year_applied'].widget = forms.TextInput(attrs={'class': 'form-control'})
