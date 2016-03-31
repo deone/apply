95687a127c9914e0082e33997efb8d0baf32c53a
Dayo Osikoya <alwaysdeone@gmail.com>
2016-03-31 21:06:29 +0100

    Make jquery datepicker work.

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,14 @@
 from django import forms
+from django.contrib.admin import widgets
 
 from app.models import PersonalInformation
 
 class PersonalInformationForm(forms.ModelForm):
+
     class Meta:
         model = PersonalInformation
-        # exclude = ['middle_name']
-        exclude = ['user']
+        exclude = ['user', 'middle_name']
+
+    def __init__(self, *args, **kwargs):
+        super(PersonalInformationForm, self).__init__(*args, **kwargs)
+        self.fields['date_of_birth'].widget = forms.TextInput(attrs={'class': 'form-control'})
