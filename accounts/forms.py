7bec7a9739c660312c5c5b8d95a196b024148541
Dayo Osikoya <alwaysdeone@gmail.com>
2016-03-31 13:05:02 +0100

    - Create login template and separate url. - Subclass AuthenticationForm as LoginForm.

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,13 @@
 from django import forms
+from django.contrib.auth.forms import AuthenticationForm
 
 from registration.forms import RegistrationForm
+
+class LoginForm(AuthenticationForm):
+    username = forms.EmailField(label='Email', max_length=254,
+        widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(label='Password',
+        widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
 class ApplyRegistrationForm(RegistrationForm):
 
--- apply/urls.py
+++ apply/urls.py
@@ -1,11 +1,15 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 from accounts.views import ApplyRegistrationView
+from accounts.forms import LoginForm
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^accounts/register/$', ApplyRegistrationView.as_view(), name='register'),
+    url(r'^accounts/create/$', ApplyRegistrationView.as_view(), name='create_account'),
+    url(r'^accounts/login/$', auth_views.login,
+      {'authentication_form': LoginForm, 'template_name': 'registration/login.html'}, name='login'),
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^payments/', include('payments.urls', namespace='payments')),
     url(r'^apply/', include('app.urls', namespace='app')),
