05249ff8a5e84a2952b35974627007a9ada2df99
Dayo Osikoya <alwaysdeone@gmail.com>
2016-04-02 22:11:50 +0100

    Compute application_completion.

--- ashesiundergrad/views.py
+++ ashesiundergrad/views.py
@@ -5,16 +5,19 @@ from setup.models import Application, UserApplication
 def get_application(slug):
     return get_object_or_404(Application, slug=slug)
 
+def compute_completion(form_filled_count, application_form_count):
+    return form_filled_count / application_form_count
+
 def index(request, orgname, slug):
     application = get_application(slug)
 
     try:
-        UserApplication.objects.get(user=request.user, application=application)
+        userapp = UserApplication.objects.get(user=request.user, application=application)
     except UserApplication.DoesNotExist:
-        UserApplication.objects.create(user=request.user, application=application)
+        userapp = UserApplication.objects.create(user=request.user, application=application)
 
     return render(request, 'ashesiundergrad/index.html',
         {
-          'application_completion': 1,
+          'application_completion': compute_completion(userapp.form_filled_count, application.applicationform_set.count()),
           'application': get_application(slug),
         })
