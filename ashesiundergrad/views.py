c174fe04c9579e52e40c6919e7ae0a1a2e4828b2
Dayo Osikoya <alwaysdeone@gmail.com>
2016-04-02 21:28:20 +0100

    - Display application home page. - Delete obsolete templates.

--- ashesiundergrad/views.py
+++ ashesiundergrad/views.py
@@ -1,4 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from setup.models import Application
+
+def get_application(slug):
+    return get_object_or_404(Application, slug=slug)
 
 def index(request, orgname, slug):
-    return render(request, 'ashesiundergrad/index.html', {'application_completion': 1})
+
+    return render(request, 'ashesiundergrad/index.html',
+        {
+          'application_completion': 1,
+          'application': get_application(slug),
+        })
