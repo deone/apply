7dc8d787236df667fdeca48c75c94cc6c21c0366
Dayo Osikoya <alwaysdeone@gmail.com>
2016-04-03 13:22:19 +0100

    - Solve errorlist not displaying issue. - Remove fieldset tag from forms.

--- setup/views.py
+++ setup/views.py
@@ -35,33 +35,6 @@ def application(request, orgname, slug):
     return render(request, template_name, get_context_variables(userapp, application))
 
 def application_form(request, orgname, slug, form_slug):
-    """ registry_key = orgname + slug.split('-')[0]
-    application = get_application(slug)
-
-    form_class =  REGISTRY[registry_key][form_slug]
-
-    if request.method == "POST":
-        print request.POST
-        form = form_class(request.POST, user=request.user, application=application)
-        if form.is_valid():
-            form.save()
-        else:
-            print form.errors
-            return redirect('application_form', orgname=orgname, slug=slug, form_slug=form_slug)
-    else:
-        form = form_class(user=request.user, application=application)
-
-    userapp = get_user_application(request.user, application)
-    template_name = '%s%s%s%s' % (registry_key, '/', form_slug, '.html')
-
-    context = get_context_variables(userapp, application)
-    context.update({
-      'form_name': unslugify(form_slug),
-      'form': form,
-      })
-
-    return render(request, template_name, context) """
-
     registry_key = orgname + slug.split('-')[0]
     form_class =  REGISTRY[registry_key][form_slug]
     application = get_application(slug)
