e1299192dae3eac580f5874b3704d23ec8ef161a
Dayo Osikoya <alwaysdeone@gmail.com>
2016-04-22 18:28:00 +0100

    Pass user application id to pay().

--- payments/views.py
+++ payments/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.sites.models import Site
 from django.core.urlresolvers import reverse
 from django.conf import settings
+
+from setup.models import UserApplication
 
 import json
 import requests
@@ -9,8 +11,11 @@ import requests
 def index(request):
     return render(request, 'payments/index.html', {})
 
-def pay(request):
+def pay(request, pk):
     current_site = Site.objects.get_current()
+    user_application = get_object_or_404(UserApplication, pk=pk)
+    org_name = user_application.application.organization.slug
+    application_slug = user_application.application.slug
     headers = {
         'Content-Type': 'application/json',
         'MP-Master-Key': settings.PAYMENT_MASTER_KEY,
@@ -20,7 +25,7 @@ def pay(request):
 
     return_url = 'http://%s%s' % (current_site.domain,
         reverse('application',
-          kwargs={'orgname': 'ashesi', 'slug': 'undergraduate-application-2016'}))
+          kwargs={'orgname': org_name, 'slug': application_slug}))
     data = '{"invoice": {"total_amount": "' + settings.APPLICATION_FEE + '", "description": "' + settings.PAYMENT_DESCRIPTION + '"}, "store": {"name": "' + settings.STORE_NAME + '"}, "actions": {"return_url": "' + return_url + '"}}'
 
     response = requests.post(settings.PAYMENT_TEST_URL, headers=headers, data=data)
