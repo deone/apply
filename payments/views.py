fa365fb9a412d982e0cdb0db241b2257a3de2745
Dayo Osikoya <alwaysdeone@gmail.com>
2016-04-22 17:35:15 +0100

    Don't hardcode return_url.

--- payments/views.py
+++ payments/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.conf import settings
+from django.contrib.sites.models import Site
 
 import json
 import requests
@@ -8,6 +9,7 @@ def index(request):
     return render(request, 'payments/index.html', {})
 
 def pay(request):
+    current_site = Site.objects.get_current()
     headers = {
         'Content-Type': 'application/json',
         'MP-Master-Key': settings.PAYMENT_MASTER_KEY,
@@ -15,7 +17,8 @@ def pay(request):
         'MP-Token': '84ca940ca8ad14a592d4',
     }
 
-    data = '{"invoice": {"total_amount": "' + settings.APPLICATION_FEE + '", "description": "' + settings.PAYMENT_DESCRIPTION + '"}, "store": {"name": "' + settings.STORE_NAME + '"}, "actions": {"return_url": "http://localhost:8000/ashesi/undergraduate-application-2016/"}}'
+    return_url = 'http://%s%s' % (current_site.domain, reverse('application', kwargs={'orgname': 'ashesi', 'slug': 'undergraduate-application-2016'}))
+    data = '{"invoice": {"total_amount": "' + settings.APPLICATION_FEE + '", "description": "' + settings.PAYMENT_DESCRIPTION + '"}, "store": {"name": "' + settings.STORE_NAME + '"}, "actions": {"return_url": "' + return_url + '"}}'
 
     response = requests.post(settings.PAYMENT_TEST_URL, headers=headers, data=data)
     obj = json.loads(response.content)
