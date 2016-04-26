0a8c6c1cfbfe519a40b8bb6a6eefbca0874a05f0
Dayo Osikoya <alwaysdeone@gmail.com>
2016-04-26 15:55:37 +0100

    - Pass application context variable to user applications template.

--- staffadmin/views.py
+++ staffadmin/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
+
+from setup.models import Application
 
 @login_required
 def index(request, orgname):
     return render(request, 'staffadmin/index.html', {})
 
+@login_required
 def user_application_list(request, orgname, pk):
-    return render(request, 'staffadmin/user_application_list.html', {})
+    application = get_object_or_404(Application, pk=pk)
+    return render(request, 'staffadmin/user_application_list.html', {'application': application})
