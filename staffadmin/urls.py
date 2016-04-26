7e1417cf3dfd9cbf754f859099cae2a9d19e2326
Dayo Osikoya <alwaysdeone@gmail.com>
2016-04-26 13:26:24 +0100

    url and view for user applications list.

--- staffadmin/urls.py
+++ staffadmin/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='home'),
+    url(r'^applications/(?P<pk>\d+)/list/$', views.user_application_list, name='user_application_list'),
 ]
--- staffadmin/views.py
+++ staffadmin/views.py
@@ -4,3 +4,6 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def index(request, orgname):
     return render(request, 'staffadmin/index.html', {})
+
+def user_application_list(request, orgname, pk):
+    return render(request, 'staffadmin/user_application_list.html', {})
