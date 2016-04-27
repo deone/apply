7d7afe030f6486dc3a4bbd4c0c12ce9184cceb4d
Dayo Osikoya <alwaysdeone@gmail.com>
2016-04-27 18:48:24 +0100

    Load user_application template from application app - in this case - ashesiundergraduate.

--- staffadmin/views.py
+++ staffadmin/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
-from setup.models import Application
+from setup.models import Application, UserApplication
+
+from utils.getters import get_registry_key
 
 @login_required
 def index(request, orgname):
@@ -9,7 +11,10 @@ def index(request, orgname):
 
 @login_required
 def user_application(request, orgname, pk):
-    return render(request, 'staffadmin/user_application.html', {})
+    user_application = get_object_or_404(UserApplication, pk=pk)
+    registry_key = get_registry_key(orgname, user_application.application.slug)
+    template_name = '%s%s%s' % (registry_key, '/', 'user_application.html')
+    return render(request, template_name, {})
 
 @login_required
 def user_application_list(request, orgname, pk):
