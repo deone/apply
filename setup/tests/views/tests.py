edaa7bbb3accc45984a81587f65f3a69863f1345
Dayo Osikoya <alwaysdeone@gmail.com>
2016-05-03 13:19:51 +0100

    Split tests into separate classes.

--- setup/tests/views/tests.py
+++ setup/tests/views/tests.py
@@ -2,13 +2,18 @@ from django.test import TestCase, Client
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
 
-from setup.models import Organization, Staff
+from setup.models import *
 
 class ViewsTests(TestCase):
 
     def setUp(self):
         self.c = Client()
         self.user = User.objects.create_user('a@a.com', 'a@a.com', '12345')
+        self.user.first_name = 'Ade'
+        self.user.last_name = 'Olu'
+        self.user.save()
+
+class HomePageTests(ViewsTests):
 
     def test_applicant_home(self):
         self.c.post(reverse('login'), {'username': 'a@a.com', 'password': '12345'})
@@ -17,11 +22,29 @@ class ViewsTests(TestCase):
         self.assertTrue('applications' in response.context)
         self.assertTemplateUsed(response, 'setup/application_list.html')
 
-    def test_applicant_home_staff(self):
-        org = Organization.objects.create(name='Ashesi College', slug='ashesi')
-        staff = Staff.objects.create(user=self.user, organization=org)
+class ApplicationTests(ViewsTests):
 
+    def setUp(self, *args, **kwargs):
+        super(ApplicationTests, self).setUp(*args, **kwargs)
+        self.org = Organization.objects.create(name='Ashesi College', slug='ashesi')
+        self.staff = Staff.objects.create(user=self.user, organization=self.org)
+        self.application = Application.objects.create(
+            organization=self.org,
+            name='Undergraduate Application',
+            year='2016',
+            is_open=True
+            )
+        form = Form.objects.create(name='Personal Information')
+        application_form = ApplicationForm.objects.create(application=self.application, slug='personal-information', form=form)
+
+    def test_applicant_home_staff(self):
         self.c.post(reverse('login'), {'username': 'a@a.com', 'password': '12345'})
         response = self.c.get(reverse('home'))
         self.assertEqual(response['location'], '/ashesi/admin/')
         self.assertEqual(response.status_code, 302)
+
+    def test_application_index(self):
+        self.c.post(reverse('login'), {'username': 'a@a.com', 'password': '12345'})
+        response = self.c.get(reverse('application', kwargs={'orgname': self.org.slug, 'slug': self.application.slug}))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'ashesiundergraduate/index.html')
