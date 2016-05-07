dccd0fadd52573c8626289723ac5ab9da7b0c081
Dayo Osikoya <alwaysdeone@gmail.com>
2016-05-07 15:32:51 +0100

    Test staffadmin views.

--- staffadmin/tests.py
+++ staffadmin/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+from setup.tests.views.tests import ViewsTests
+
+class StaffadminViewsTests(ViewsTests):
+
+    def test_index(self):
+        self.login()
+        response = self.c.get(reverse('staffadmin:home', kwargs={'orgname': 'ashesi'}))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'staffadmin/index.html')
+
+    def test_user_application(self):
+        self.login()
+        response = self.c.get(reverse('staffadmin:user_application', kwargs={'orgname': 'ashesi', 'pk': self.user_app.pk}))
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('user_application' in response.context)
+        self.assertTemplateUsed(response, 'ashesiundergraduate/user_application.html')
+
+    def test_user_application_list(self):
+        self.login()
+        response = self.c.get(reverse('staffadmin:user_application_list', kwargs={'orgname': 'ashesi', 'pk': self.application.pk}))
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('application' in response.context)
+        self.assertTemplateUsed(response, 'staffadmin/user_application_list.html')
