a3ef7518c86ae9327b008d70fac225ae009deb40
Dayo Osikoya <alwaysdeone@gmail.com>
2016-05-09 23:29:57 +0100

    Leave AutoSlugField stub since some migrations depend on it.

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,8 +2,24 @@
 
 from __future__ import unicode_literals
 
-from django import forms
+from django.db.models.fields import SlugField
 from django.utils.translation import ugettext_lazy as _
+from django import forms
+
+class AutoSlugField(SlugField):
+    """
+    Auto populates itself from another field.
+
+    It behaves like a regular SlugField.
+    When populate_from is provided it'll populate itself on creation,
+    only if a slug was not provided.
+
+    P.S We don't use this anymore, but have to leave a stub because some
+    migrations depend on it.
+    """
+
+    def __init__(self, *args, **kwargs):
+        pass
 
 def validate_phone_number(number):
     if not number.startswith('+'):
